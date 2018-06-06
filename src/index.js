6c15301204dc4a784e8b513d452dd553b982c750
Garrett Knox <garrettknox@garretts-mbp-2.attlocal.net>
2018-06-05 22:37:05 -0500

    Create Initial Middleware
    Defined the logger middleware function to log any action that is
    dispatched to the Redux Store along with the new state of the store
    following that dispatch. Also included the Redux Thunk middleware for
    future use

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import ReactDOM from 'react-dom'
 
 // Redux imports
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 // Custom imports
 import './index.css'
 import App from './components/App'
 import reducers from './reducers'
+import middleware from './middleware'
 
-const store = createStore(reducers)
+// Create the Redux Store using the defined Root Reducer and Middleware function(s)
+const store = createStore(reducers, middleware)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+	, document.getElementById('root'))
